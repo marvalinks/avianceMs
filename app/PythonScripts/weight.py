1a18c5d638b2f20db4a73e2ef32ac29683e346fb
Qhobeey Martison <bludohric@gmail.com>
2022-03-28 12:12:52 +0000

    modification v2.0

--- app/PythonScripts/weight.py
+++ app/PythonScripts/weight.py
@@ -5,16 +5,16 @@ import random
 
 
 def w2():
-    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    print(random.choice(list1))
+    # list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # print(random.choice(list1))
 
-    # serialBout = serial.Serial()
-    # serialBout.port = "COM5"
-    # serialBout.open()
+    serialBout = serial.Serial()
+    serialBout.port = "COM5"
+    serialBout.open()
 
-    # packet = serialBout.readline()
-    # wtt = packet.decode("utf").rstrip("\n")
-    # print(wtt)
+    packet = serialBout.readline()
+    wtt = packet.decode("utf").rstrip("\n")
+    print(wtt)
 
 
 w2()
