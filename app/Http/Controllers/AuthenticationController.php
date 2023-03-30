4a75915cc0eb99acd4d018a236cf39885b0d9d93
qhobeey <bludohric@gmail.com>
2023-03-30 13:29:00 +0000

    updates v1.6

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -30,6 +30,7 @@ class AuthenticationController extends Controller
             // dd($email, $request->all());
             if (Auth::attempt(['email' => $email, 'password' => $request->password])) {
                 $request->session()->regenerate();
+                dd('op');
                 return redirect()->route('backend.dashboard');
             }
     
